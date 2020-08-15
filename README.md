70d260a7762242de6c2f7322b1ed683d5575c7ff
Ishrat Rahman Sami <ishrat@thebyte9.com>
2020-08-15 13:38:32 +0100

    Get latest feed content

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# feed-data-generator
-Copy rss feed in local drive
+# Feed data generator
+
+Feed data generator is a simple crawler script that fetches a set of [BBC RSS](https://www.bbc.co.uk/news/10628494) feeds and read the latest articles and save then in /data/content/ using the JSON format. Fetched RSS files will be saved in /data/rss/ folder using the JSON format.
+
+Use the following command to run the script:
+
+```bash
+python3 feed.py 
+```
---
+++ feed.py
@@ -0,0 +1,106 @@
+import traceback
+import urllib3,  urllib
+import xmltodict, json
+from collections import OrderedDict
+import sys, re
+import os.path
+from bs4 import BeautifulSoup
+from datetime import date
+
+dataPath = './data'
+urls = [
+    "http://feeds.bbci.co.uk/news/rss.xml",
+    "http://feeds.bbci.co.uk/news/world/rss.xml",
+    "http://feeds.bbci.co.uk/news/uk/rss.xml",
+    "http://feeds.bbci.co.uk/news/business/rss.xml",
+    "http://feeds.bbci.co.uk/news/politics/rss.xml",
+    "http://feeds.bbci.co.uk/news/health/rss.xml",
+    "http://feeds.bbci.co.uk/news/education/rss.xml",
+    "http://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+    "http://feeds.bbci.co.uk/news/technology/rss.xml",
+    "http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml"
+];
+contentClass = "story-body__inner"
+today = date.today();
+
+def saveFile(content, filePath):
+    file = open(filePath, "w")
+    file.write(content)
+    file.close()
+    return
+
+def saveJsonFile(content, filePath):
+    with open(filePath, 'w') as f:
+        json.dump(content, f)
+    return
+
+def getPageContent(link):
+    fp = urllib.request.urlopen(link)
+    mybytes = fp.read()
+    page = mybytes.decode("utf8")
+    fp.close()
+    soup = BeautifulSoup(page, features="html.parser")
+    divs = soup.findAll('div',attrs={"class":contentClass})
+    text = '';
+    for div in divs:
+        for item in div.findChildren():
+            if item.name in ['p', 'ul', 'li', 'ol']:
+                text += str(item.text) + ' '
+                
+    return text
+
+def getFileName(processedItem):
+    name = processedItem['pubDate'] + processedItem['title']
+    name = name.replace(' ', '')
+    name = name.replace(':', '')
+    name = name.replace(',', '')
+    return name
+
+def getFeedName(url):
+    name = url.replace('http://', '')
+    name = name.replace('https://', '')
+    name = name.replace('/', '-')
+    name = name.replace('.', '')
+    name = today.strftime("%Y%m%d-") + name + '.json'
+    return name
+
+def saveContent(rssItem):
+    processedItem = {}
+    
+    for key in rssItem.keys():
+        if (key in ['title', 'description', 'link', 'pubDate']):
+            processedItem[key] = rssItem[key]
+            
+    processedItem['content'] = getPageContent(processedItem['link'], )
+    filename = getFileName(processedItem);
+    path = os.path.join(dataPath, 'content', filename + ".json") 
+    saveJsonFile(processedItem, path)
+    return
+
+def getResponse(url):
+    http = urllib3.PoolManager()
+    return http.request('GET', url)
+
+def saveXmlToItems(url):
+    response = getResponse(url)
+    try:
+        data = xmltodict.parse(response.data)
+        filePath = os.path.join(dataPath, 'rss', getFeedName(url)) 
+        saveJsonFile(data['rss']['channel'], filePath)
+        
+        data = data['rss']['channel']['item']
+        for item in data:
+            saveContent(item)
+
+    except:
+        print("Failed to parse xml from response (%s)" % traceback.format_exc())
+        
+    return
+
+
+if __name__ == "__main__":
+    for url in urls:
+        print('Processing ' + url)
+        saveXmlToItems(url)
+
+    print('Finishing')
